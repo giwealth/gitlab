638c0715ae1f54f1216a77e1ca6b2518c1a5150b
zengming <zengming@juwang.cn>
2019-09-16 18:16:26 +0800

    update

--- gitlab.go
+++ gitlab.go
@@ -1,20 +1,34 @@
 package gitlab
 
 import (
+	"strconv"
+	"io/ioutil"
+	"encoding/json"
 	// "strings"
 	"fmt"
 	"net/http"
+	"net/url"
 )
 
 // Client gitlab api client
 type Client struct {
 	BaseURL     string
 	AccessToken string
+	APIVersionPath string
+}
+
+// New create gitlab client
+func New(baseURL, accessToken string) Client {
+	return Client{
+		BaseURL: baseURL,
+		AccessToken: accessToken,
+		APIVersionPath: "/api/v4",
+	}	
 }
 
 // httpRequest http请求，返回response
-func httpGetRequest(url, token string, c *http.Client) (*http.Response, error) {
-	req, err := http.NewRequest("GET", url, nil)
+func httpGetRequest(urlAddr, token string, c *http.Client) (*http.Response, error) {
+	req, err := http.NewRequest("GET", urlAddr, nil)
 	if err != nil {
 		return nil, err
 	}
@@ -30,4 +44,66 @@ func httpGetRequest(url, token string, c *http.Client) (*http.Response, error) {
 	}
 
 	return resp, err
+}
+
+// GetAPI get gitlab api
+func (c *Client) GetAPI(addr string, v interface{}) error {
+	var list []interface{}
+	page := 1
+	for {
+		u, err := url.Parse(addr)
+		if err != nil {
+			return err
+		}
+		q := u.Query()
+		q.Set("page", fmt.Sprint(page))
+		u.RawQuery = q.Encode()
+
+		httpClient := &http.Client{}
+
+		req, err := http.NewRequest("GET", u.String(), nil)
+		if err != nil {
+			return nil
+		}
+
+		req.Header.Set("Private-Token", c.AccessToken)
+
+		res, err := httpClient.Do(req)
+		if err != nil {
+			return err
+		}
+		defer res.Body.Close()
+
+		if res.StatusCode < 200 || res.StatusCode > 299 {
+			return fmt.Errorf("request error response status code %v", res.StatusCode)
+		}
+
+		var l []interface{}
+		f, err := ioutil.ReadAll(res.Body)
+		if err != nil {
+			return err
+		}
+
+		err = json.Unmarshal(f, &l)
+		if err != nil {
+			return err
+		}
+
+		list = append(list, l...)
+
+		totalPages, err := strconv.Atoi(res.Header.Get("X-Total-Pages"))
+		if err != nil {
+			return err
+		}
+
+		if page == totalPages {
+			break
+		}
+
+		page++
+	}
+
+	v = list
+
+	return nil
 }
